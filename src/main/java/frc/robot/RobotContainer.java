68070fb0a237f03b79defcbab9dabe5fa7dc3cb9
GrahamSH <grahamshllk@gmail.com>
2023-10-18 10:32:57 -0400

    Initial commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,89 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.print.attribute.standard.MediaSize.NA;
+
+import com.fasterxml.jackson.core.sym.Name;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.NavX;
+
+import frc.robot.commands.JoystickDrive;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final NavX navx = new NavX();
+  private final DriveTrain drive = new DriveTrain(navx);
+
+  public static Joystick joystick = new Joystick(Constants.OperatorConstants.kDriverJoystickPort);
+
+  private static XboxController m_driverController =
+      new XboxController(OperatorConstants.kDriverControllerPort);
+
+  private final JoystickDrive joystickDrive = new JoystickDrive(drive);
+
+    
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+
+    // Configure the trigger bindings
+    configureBindings();
+    drive.setDefaultCommand(joystickDrive);
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    //new Trigger(m_exampleSubsystem::exampleCondition)
+    //    .onTrue(new ExampleCommand(m_exampleSubsystem));
+    
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+    JoystickButton wheelsX = new JoystickButton(m_driverController, XboxController.Button.kY.value);
+    wheelsX.onTrue(Commands.run(() -> drive.setX()));
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+      return new PathPlannerAuto("New Auto"); 
+    // An example command will be run in autonomous
+   // return Autos.exampleAuto(m_exampleSubsystem);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/NavX.java
@@ -0,0 +1,51 @@
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class NavX extends SubsystemBase {
+
+    public final AHRS ahrs = new AHRS(SPI.Port.kMXP);
+
+    public NavX() {
+        ahrs.reset();
+        ahrs.calibrate();
+    }
+
+    /*
+     * Deprecated: Use .ahrs.getPitch()
+     */
+    public double getPitch() {
+        return ahrs.getPitch();
+    }
+
+    /*
+     * Deprecated: Use .ahrs.getYaw()
+     */
+    public double getYaw() {
+        return ahrs.getYaw();
+    }
+
+    /*
+     * Deprecated: Use .ahrs.zeroYaw()
+     */
+    public void resetYaw() {
+        ahrs.zeroYaw();
+    }
+
+    /*
+     * Deprecated: Use .ahrs.getRate()
+     */
+    public double getRate() {
+        return ahrs.getRate();
+    }
+
+    /*
+     * Deprecated: Use .ahrs.getRotation2d()
+     */
+    public Rotation2d getRotation2d() {
+        return ahrs.getRotation2d();
+    }
+}
