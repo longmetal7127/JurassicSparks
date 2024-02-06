13f82980f03b6348139e5cafa384dd410c6a2771
griffbopat <146395902+longmetal-drivestation@users.noreply.github.com>
2024-02-05 19:12:52 -0500

    Create ClimbTrain.java

---
+++ src/main/java/frc/robot/subsystems/ClimbTrain.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkAbsoluteEncoder;
+import com.revrobotics.SparkPIDController;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.ModuleConstants;
+import monologue.Logged;
+
+public class ClimbTrain {
+    if (getXButtonPuressed = True){
+        System.out.println("test");
+    }
+    else{
+        System.out.println("fail");
+    }
+}
