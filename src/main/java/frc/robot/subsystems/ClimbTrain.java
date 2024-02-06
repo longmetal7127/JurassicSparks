fb6e18c3c1ce333098f111b61d9b6eb8a2cb0a19
griffbopat <146395902+longmetal-drivestation@users.noreply.github.com>
2024-02-05 19:29:46 -0500

    jACK IS THE BEST

--- src/main/java/frc/robot/subsystems/ClimbTrain.java
+++ src/main/java/frc/robot/subsystems/ClimbTrain.java
@@ -4,17 +4,20 @@ import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkAbsoluteEncoder;
 import com.revrobotics.SparkPIDController;
+import edu.wpi.first.wpilibj.XboxController;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 import frc.robot.Constants.ModuleConstants;
 import monologue.Logged;
 
-public class ClimbTrain {
-    if (getXButtonPuressed = True){
-        System.out.println("test");
-    }
-    else{
-        System.out.println("fail");
+public class ClimbTrain extends XboxController{
+    public ClimbTrain() {
+        if (getXButtonPressed() == true){
+            System.out.println("test");
+        }
+        else{
+            System.out.println("fail");
+        }
     }
 }
